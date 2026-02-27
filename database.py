fc44985a8e45fc5b7ff58db5f45e8249e7930a81
georgiy-37 <goltugea@gmail.com>
2026-02-27 22:11:51 +0300

    dockerfiles

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 
 # Для PostgreSQL здесь будет: "postgresql+asyncpg://user:pass@localhost/dbname"
-DATABASE_URL = "sqlite+aiosqlite:///./data/booking.db"
+DATABASE_URL = "sqlite+aiosqlite:////app/data/booking.db"
+
 
 engine = create_async_engine(DATABASE_URL)
 AsyncSessionLocal = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
